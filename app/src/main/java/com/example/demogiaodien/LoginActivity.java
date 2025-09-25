a73bfddac292aaabeb2d95f9fc6a48207d24d7cf
Phanga05 <nguyenphucan101105@gmail.com>
2025-09-25 20:26:22 +0700

    Xong đăng nhập

--- app/src/main/java/com/example/demogiaodien/LoginActivity.java
+++ app/src/main/java/com/example/demogiaodien/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.demogiaodien;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,13 @@ public class LoginActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            TextView btn = findViewById(R.id.textViewSignup);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                }
+            });
 
             return insets;
         });
